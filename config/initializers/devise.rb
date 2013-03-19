1d535de5f97069ff3d94381d1ba8bd990464c2b6
Logic-Seeker <shobhabstha@gmail.com>
2013-03-19 16:26:00 +0545

    minor change

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -113,8 +113,8 @@ Devise.setup do |config|
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "shobhabstha@gmail.com",
-  :password             => "shresthas01",
+  :user_name            => "",
+  :password             => "",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
